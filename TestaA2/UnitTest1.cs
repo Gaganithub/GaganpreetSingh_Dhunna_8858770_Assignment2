9e04d6b74d7926372349c1f58124f7b07868dbe2
Gaganpreet singh dhunna <Gdhunna8770@conestogac.on.ca>
2023-05-31 17:05:10 -0400

    Add project files.

---
+++ TestaA2/UnitTest1.cs
@@ -0,0 +1,56 @@
+using TriangleSolver;
+
+namespace TestaA2
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test_AnalyzeTriangle_inpput25and25and25_EQUILATERAL()
+        {   // 1 A
+            int input1 = 25;
+            int input2 = 25;
+            int input3 = 25;
+            String expectResult = "The triangle is valid and is an EQUILATERAL";
+
+            // 2 A
+            string ret = Triangle.AnalyzeTriangle(input1, input2, input3);
+
+            //3 A
+            StringAssert.AreEqualIgnoringCase(expectResult,ret);
+        }
+
+        [Test]
+        public void Test_AnalyzeTriangle_inpput25and25and25_EQUILATERAL()
+        {   // 1 A
+            int input1 = 25;
+            int input2 = 25;
+            int input3 = 25;
+            String expectResult = "The triangle is valid and is an EQUILATERAL";
+
+            // 2 A
+            string ret = Triangle.AnalyzeTriangle(input1, input2, input3);
+
+            //3 A
+            StringAssert.AreEqualIgnoringCase(expectResult, ret);
+        }
+        [Test]
+        public void Test_AnalyzeTriangle_inpput25and25and25_EQUILATERAL()
+        {   // 1 A
+            int input1 = 25;
+            int input2 = 25;
+            int input3 = 25;
+            String expectResult = "The triangle is valid and is an EQUILATERAL";
+
+            // 2 A
+            string ret = Triangle.AnalyzeTriangle(input1, input2, input3);
+
+            //3 A
+            StringAssert.AreEqualIgnoringCase(expectResult, ret);
+        }
+    }
+}
