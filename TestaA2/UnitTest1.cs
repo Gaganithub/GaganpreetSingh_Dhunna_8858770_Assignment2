76056305d1f06492c628dfe3fa81402a0864724b
Gaganpreet singh dhunna <Gdhunna8770@conestogac.on.ca>
2023-05-31 17:06:25 -0400

    write 2 test

--- TestaA2/UnitTest1.cs
+++ TestaA2/UnitTest1.cs
@@ -38,19 +38,6 @@ namespace TestaA2
             //3 A
             StringAssert.AreEqualIgnoringCase(expectResult, ret);
         }
-        [Test]
-        public void Test_AnalyzeTriangle_inpput25and25and25_EQUILATERAL()
-        {   // 1 A
-            int input1 = 25;
-            int input2 = 25;
-            int input3 = 25;
-            String expectResult = "The triangle is valid and is an EQUILATERAL";
-
-            // 2 A
-            string ret = Triangle.AnalyzeTriangle(input1, input2, input3);
-
-            //3 A
-            StringAssert.AreEqualIgnoringCase(expectResult, ret);
-        }
+       
     }
 }
