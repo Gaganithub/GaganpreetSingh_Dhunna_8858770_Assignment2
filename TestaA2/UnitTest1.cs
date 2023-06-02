07aff0a4f83f5a700395a9313b640814b9663ee9
Gaganpreet singh dhunna <Gdhunna8770@conestogac.on.ca>
2023-06-01 23:31:39 -0400

    Isosceles test 1

--- TestaA2/UnitTest1.cs
+++ TestaA2/UnitTest1.cs
@@ -10,7 +10,7 @@ namespace TestaA2
         }
 
         [Test]
-        public void Test_AnalyzeTriangle_inpput25and25and25_EQUILATERAL()
+        public void Test_AnalyzeTriangle_inpput40and40and40_EQUILATERAL()
         {   // 1 A
             int input1 = 40;
             int input2 = 40;
@@ -24,7 +24,22 @@ namespace TestaA2
             StringAssert.AreEqualIgnoringCase(expectResult,ret);
         }
 
-      
-       
+        [Test]
+        public void Test_AnalyzeTriangle_inpput60and60and70_ISOSCELES()
+        {   // 1 A
+            int input1 = 60;
+            int input2 = 60;
+            int input3 = 70;
+            String expectResult = "The triangle is valid and is an ISOSCELES";
+
+            // 2 A
+            string ret = Triangle.AnalyzeTriangle(input1, input2, input3);
+            Console.WriteLine(ret);
+            //3 A
+            StringAssert.AreEqualIgnoringCase(expectResult, ret);
+        }
+
+
+
     }
 }
